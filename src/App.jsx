a6a66c7f53ad9f5caea6713a44c44aaec7a5516b
pomazkovanton <vitrumbeta@gmail.com>
2022-08-11 10:06:48 +0800

    fix: удалил не используемые переменые

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,6 @@ import data from './tickets.json';
 import Header from "./componets/Header/Header";
 import Main from "./componets/Main/Main";
 import TicketList from "./componets/TicketList/TicketList";
-import RadioGroup from "./componets/UI/RadioGroup/RadioGroup";
-import CheckboxGroup from "./componets/UI/CheckboxGroup/CheckboxGroup";
-import Fieldset from "./componets/UI/Fieldset/Fieldset";
 import Card from "./componets/UI/Card/Card";
 import FormFilters from "./componets/FormFilters/FormFilters";
 
